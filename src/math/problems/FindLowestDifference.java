2299e224dbabeeb53098cf98af520c437ad9ae94
kttheman1 <104694182+kttheman1@users.noreply.github.com>
2022-05-18 12:49:14 -0400

    string problems checkpoint

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -7,30 +7,10 @@ public class FindLowestDifference {
          Read this below two array. Find the lowest difference between the two array cell.
          The lowest difference between cells is 1
         */
-      //  int [] array1 = {30,12,5,9,2,20,33,1};
-       // int [] array2 = {18,25,41,47,17,36,14,19};
+      int [] array1 = {30,12,5,9,2,20,33,1};
+      int [] array2 = {18,25,41,47,17,36,14,19};
 
-  //  {
-         //   Arrays.sort(array1[]);
-           // Arrays.sort(array2[]);
-//
- //           int a = 0;
-   //         int b = 0;
 
-     //       int result = Integer.MAX_VALUE;
-
-//            while (a < m && b < n) {
-  //              if (Math.abs(array1[a] - array2[b]) < result)
-    //                result = Math.abs(array1[a] - array2[b]);
-//
-  //              if (array1[a] < array2[b])
-    //                a++;
-      //          else
-        //            b++;
-          //      System.out.println(lowestDifference (array1, array2, m, n));
-          //  }
-            //return result;
-        //}
 
 
             }
--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -1,11 +1,29 @@
 package string.problems;
-
+import java.util.*;
 public class Palindrome {
     public static void main(String[] args) {
         /*
           If a String is reversed and it remains unchanged, that is called Palindrome. For example, MOM,DAD,MADAM are
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter word here: ");
+        String word = input.nextLine();
+        String reverseWord = "";
+
+        int wordLength = word.length();
+
+        for (int i = (wordLength - 1); i >= 0; --i) {
+            reverseWord = reverseWord + word.charAt(i);
+        }
+        if (word.toLowerCase().equals(reverseWord.toLowerCase())) {
+            System.out.println(word + " is a Palindrome.");
+        }
+        else {
+            System.out.println(word + " is not a Palindrome.");
+        }
 
     }
 }
